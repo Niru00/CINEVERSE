dc745cb745063a9984e6ad716e7868ac36f272d4
Niru00 <nirupambarman82@gmail.com>
2026-03-15 08:09:18 +0530

    mail service fixed

--- src/services/mail.service.js
+++ src/services/mail.service.js
@@ -4,9 +4,11 @@ dotenv.config()
 
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+   host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
-       user: process.env.EMAIL,          
+    user: process.env.EMAIL,
     pass: process.env.EMAIL_PASSWORD,
   },
 })
